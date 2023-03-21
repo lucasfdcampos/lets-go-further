61faa7602434b9d7270027522e07c7a11d2a5b32
lucasfdcampos <lucascampos.ads@gmail.com>
2023-03-21 18:49:16 -0300

    envelope

--- greenlight/cmd/api/helpers.go
+++ greenlight/cmd/api/helpers.go
@@ -9,6 +9,9 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+// Define an envelope type.
+type envelope map[string]any
+
 // Retrieve the "id" URL parameter from the current request context, then convert it to
 // an integer and return it. If the operation isn't successful, return 0 and an error.
 func (app *application) readIDParam(r *http.Request) (int64, error) {
