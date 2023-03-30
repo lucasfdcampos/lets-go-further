3c114f7747c7d28ae9650ffaff23dac6792ae402
lucasfdcampos <lucascampos.ads@gmail.com>
2023-03-30 13:06:34 -0300

    Dynamic filtering

--- greenlight/internal/data/movies.go
+++ greenlight/internal/data/movies.go
@@ -214,6 +214,8 @@ func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*M
 	query := `
 		SELECT id, created_at, title, year, runtime, genres, version
 		FROM movies
+		WHERE (LOWER(title) = LOWER($1) OR $1 = '')
+		AND (genres @> $2 OR $2 = '{}')
 		ORDER BY id`
 
 	// Create a context with a 3-second timeout.
@@ -222,7 +224,7 @@ func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*M
 
 	// Use QueryContext() to execute the query. This returns a sql.Rows resultset
 	// containing the result.
-	rows, err := m.DB.QueryContext(ctx, query)
+	rows, err := m.DB.QueryContext(ctx, query, title, pq.Array(genres))
 	if err != nil {
 		return nil, err
 	}
