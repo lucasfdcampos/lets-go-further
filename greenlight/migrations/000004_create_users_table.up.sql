8fef35d5ce0e7b5a78d5053a02ea868eafb113cf
lucasfdcampos <lucascampos.ads@gmail.com>
2023-04-10 08:33:12 -0300

    register user

--- greenlight/migrations/000004_create_users_table.up.sql
+++ greenlight/migrations/000004_create_users_table.up.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS users (
   created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
   name text NOT NULL,
   email citext UNIQUE NOT NULL,
-  passwortd_hash bytea NOT NULL,
+  password_hash bytea NOT NULL,
   activated bool NOT NULL,
   version integer NOT NULL DEFAULT 1
 );
